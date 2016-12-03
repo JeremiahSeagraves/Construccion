efd3f67807fdad6cd431506c6aa6ff4536432481
juan <juan@192.168.1.69>
2016-12-02 20:44:15 -0600

    Ya funcionan las ventas con referencias a los empleados, ventas y detalles venta, agrego el archivo .sql para importar desde PHPMyAdmin

---
+++ Construccion/src/Modelo/Venta.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+/**
+ *
+ * @author rodrigopeniche
+ */
+public class Venta {
+    
+    private static int contadorVentas = 0;
+    private final int clave;
+    private final ArrayList<Articulo> articulosVendidos;
+    private double montoVenta;
+    private double ganancia;
+    private String fecha;
+    private Empleado empleado;
+
+  
+    public Venta(ArrayList<Articulo> articulosVendidos){
+        this.articulosVendidos= articulosVendidos;
+        Date fechaActual = new Date();
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyyMMdd");
+        fecha = formatoFecha.format(fechaActual);
+        clave = ++contadorVentas;
+    }
+
+    public ArrayList<Articulo> getArticulosVendidos() {
+        return articulosVendidos;
+    }
+    
+    public double getMontoVenta() {
+        return montoVenta;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public double getGanancia() {
+        return ganancia;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+    
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+    
+    public void setMonto(double monto){
+        this.montoVenta= monto;
+    }
+
+    public void setGanancia(double ganancia) {
+        this.ganancia = ganancia;
+    }
+    
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+    
+    public void setFecha(String fecha){
+        this.fecha = fecha;
+    }
+    
+       
+}
